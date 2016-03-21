c0ce8dc08f7ac9b81ed1322569430ce12f0e7083
Lex Myers <lex.myers@gmail.com>
2016-03-21 16:58:38 -0700

    attempt at adding auth

--- models/User.js
+++ models/User.js
@@ -1,9 +1,17 @@
+var bcrypt = require('bcrypt');
+
+
 module.exports = (mongoose, models) => {
   var Schema = mongoose.Schema;
   var userSchema = new mongoose.Schema({
     name: String,
+    password: String,
     files: [{type: mongoose.Schema.Types.ObjectId, ref: 'File'}]
   });
+  userSchema.pre('save', function(next) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10))
+  })
+
   var User = mongoose.model('User', userSchema);
   models.User = User;
 }
