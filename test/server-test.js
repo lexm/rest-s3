97c5be315ff4c70efdc83cf0a45e1e03b2ef1dec
Lex Myers <lex.myers@gmail.com>
2016-03-21 21:15:36 -0700

    added tests for listing new user and posting new file

--- test/server-test.js
+++ test/server-test.js
@@ -15,4 +15,42 @@ var File = models.File;
 
 require(__dirname + '/../server');
 
-// describe()
+var newUser = 'sally';
+
+describe('testing user API', function(){
+  // it('should be able to add a new user', function(done) {
+  //   request('localhost:3000')
+  //   .post('/users')
+  //   .send({name: newUser, files: []})
+  //   .end(function(err, res) {
+  //     expect(err).to.eql(null);
+  //     expect(res.text).to.eql('Welcome ' + newUser);
+  //     done();
+  //   })
+  // });
+  it('should be able to list new user', function(done) {
+    request('localhost:3000')
+    .get('/users/' + newUser)
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.length).to.not.eql(0);
+      expect(res.body[0].name).to.eql(newUser);
+      expect(res.body[0].files).to.eql([]);
+      expect(res.body[0]._id).to.not.be.null;
+      done();
+    });
+  });
+  it('should be able to post a file for new user', function(done) {
+    request('localhost:3000')
+    .post('/users/' + newUser + '/files')
+    .send({'fileName': 'thisFile.txt', 'content': 'here\'s some content'})
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.text).to.eql('file thisFile.txt added for user ' + newUser);
+      done();
+    });
+  });
+  it('should be able to list files owned by user', function(done) {
+    done();
+  });
+});
